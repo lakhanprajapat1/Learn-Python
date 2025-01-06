da69b8499aaefcb63a4fcce89300d058a40cdd62
lakhanprajapat1 <lakhan10209@gmail.com>
2025-01-06 09:05:12 +0530

    learn

--- first.py
+++ first.py
@@ -1,59 +1,71 @@
-print("Day_1")
+# print("Day_1")
 
 
 
-print(23)
-name="lakhan"
-age=19
-city= "delhi"
-print("my name is ", name)
-print("i am from ", city)
-print("my age is", age)
+# print(23)
+# name="lakhan"
+# age=19
+# city= "delhi"
+# print("my name is ", name)
+# print("i am from ", city)
+# print("my age is", age)
 
-age2=age
-print(age)
-
-
-
-#Arithmatic operators
-a=10
-b=20
-print( "the sum is: ",a+b)
-print(a-b)
-print(a*b)
-print(a/b)
-print(a%b)
-print(a**b)
-
-# relational operators
-a=20
-b=50
-print(a==b) #false
-print(a!=b) #true
-print(a<b) #true
-print(a>b) #false
-print(a<=b) #true
-print(a>=b) #false
+# age2=age
+# print(age)
 
 
-# assignment operator
-num=10
-num +=10
-print("num is:",num) #20
 
-#now num is 20
-num-=10
-print("num is:",num) #10
+# #Arithmatic operators
+# a=10
+# b=20
+# print( "the sum is: ",a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+# # relational operators
+# a=20
+# b=50
+# print(a==b) #false
+# print(a!=b) #true
+# print(a<b) #true
+# print(a>b) #false
+# print(a<=b) #true
+# print(a>=b) #false
 
 
-# logical operators
-a=10
-b=20
+# # assignment operator
+# num=10
+# num +=10
+# print("num is:",num) #20
+
+# #now num is 20
+# num-=10
+# print("num is:",num) #10
+
+
+# # logical operators
+# a=10
+# b=20
 # print(not (a<20)) # false because not operatore output ko ulta kr deta h
 # print(not (a>20))
 
-print((a<b) and (a==0)) #both are true then return true
-print((a<b) or (a>0)) #anyone is true the return  true
+# print((a<b) and (a==0)) #both are true then return true
+# print((a<b) or (a>0)) #anyone is true the return  true
+
+## type conversion
+# a='2'
+# b=7.89
+# print(type(a)) 
+# print(type(b))
+# print(int(a)+b) #type casting manually conversion 
+
+
+## input 
+# name = input("enter your name :")
+# print("welcome!",name)
 
 
 
@@ -63,15 +75,12 @@ print((a<b) or (a>0)) #anyone is true the return  true
 
 
 
+# x=2
+# y=3
+# print("the sum of x and y is : ",x+y)
+
+# def greet(e):
+#     print(e)
 
 
-
-x=2
-y=3
-print("the sum of x and y is : ",x+y)
-
-def greet(arg):
-    print(arg)
-
-
-greet("hello coders")
+# greet("hello coders")
---
+++ practice/practice1.py
@@ -0,0 +1,21 @@
+# give 2 input and print their sum 
+num1=int(input("enter first num: "))
+num2=int(input("enter second num: "))
+sum=num1+num2
+print("the sum is:",sum)
+
+# print area of squre
+side=int(input("enter the side of squre: "))
+area=side*side
+print("the area of squre is:",area)
+
+# avg of two input float values
+num1=float(input("enter first num: "))
+num2=float(input("enter second num: "))
+avg=(num1+num2)/2
+print("avg of values is:",avg)
+
+#wap to print true if a is greater then and equal to b and print false
+a=int(input("enter first num"))
+b=int(input("enter second num"))
+print(a>=b) #false
