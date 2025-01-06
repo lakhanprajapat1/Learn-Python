b150caccf454ce1e873e16064d4e50bc39c324c1
lakhanprajapat1 <lakhan10209@gmail.com>
2025-01-06 09:09:57 +0530

    learn

--- first.py
+++ first.py
@@ -1,86 +1,68 @@
-# print("Day_1")
+print("Day_1")
 
 
 
-# print(23)
-# name="lakhan"
-# age=19
-# city= "delhi"
-# print("my name is ", name)
-# print("i am from ", city)
-# print("my age is", age)
+print(23)
+name="lakhan"
+age=19
+city= "delhi"
+print("my name is ", name)
+print("i am from ", city)
+print("my age is", age)
 
-# age2=age
-# print(age)
-
-
-
-# #Arithmatic operators
-# a=10
-# b=20
-# print( "the sum is: ",a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
-# print(a%b)
-# print(a**b)
-
-# # relational operators
-# a=20
-# b=50
-# print(a==b) #false
-# print(a!=b) #true
-# print(a<b) #true
-# print(a>b) #false
-# print(a<=b) #true
-# print(a>=b) #false
-
-
-# # assignment operator
-# num=10
-# num +=10
-# print("num is:",num) #20
-
-# #now num is 20
-# num-=10
-# print("num is:",num) #10
-
-
-# # logical operators
-# a=10
-# b=20
-# print(not (a<20)) # false because not operatore output ko ulta kr deta h
-# print(not (a>20))
-
-# print((a<b) and (a==0)) #both are true then return true
-# print((a<b) or (a>0)) #anyone is true the return  true
-
-## type conversion
-# a='2'
-# b=7.89
-# print(type(a)) 
-# print(type(b))
-# print(int(a)+b) #type casting manually conversion 
-
-
-## input 
-# name = input("enter your name :")
-# print("welcome!",name)
+age2=age
+print(age)
 
 
 
+#Arithmatic operators
+a=10
+b=20
+print( "the sum is: ",a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+
+# relational operators
+a=20
+b=50
+print(a==b) #false
+print(a!=b) #true
+print(a<b) #true
+print(a>b) #false
+print(a<=b) #true
+print(a>=b) #false
 
 
+# assignment operator
+num=10
+num +=10
+print("num is:",num) #20
+
+#now num is 20
+num-=10
+print("num is:",num) #10
 
 
+# logical operators
+a=10
+b=20
+print(not (a<20)) # false because not operatore output ko ulta kr deta h
+print(not (a>20))
+
+print((a<b) and (a==0)) #both are true then return true
+print((a<b) or (a>0)) #anyone is true the return  true
+
+# type conversion
+a='2'
+b=7.89
+print(type(a)) 
+print(type(b))
+print(int(a)+b) #type casting manually conversion 
 
 
-# x=2
-# y=3
-# print("the sum of x and y is : ",x+y)
-
-# def greet(e):
-#     print(e)
-
-
-# greet("hello coders")
+# input 
+name = input("enter your name :")
+print("welcome!",name)
